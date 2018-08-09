d8abdb3d1afa0ab88fa1dfa5cd00133f845a876f
Yasufumi Moriya <yasufumi.moriya@adaptcentre.ie>
2018-08-09 18:12:28 +0100

    linear_regression: Cleaning code to make it more understandable
    Add more comments
    change mean_sum_squared_error(?) to mean_squared_error
    change input shape of train function

--- code/linear_regression.py
+++ code/linear_regression.py
@@ -12,37 +12,40 @@ class LinearRegression:
         self.errors = list()
         self._is_univariate = True if n_in == 1 else False
 
-    def train(self, data, n_iter=1000, lr=0.01):
+    def train(self, x, y, n_iter=1000, lr=0.01):
         """train a linear regression model with gradient descent
-           data: a tuple of data points and labels
+           x: ndarray storing data points of n_in features
+           y: ndarray storing labels of x
            n_iter: the number of iterations
            lr: learning rate
         """
-        x, y = data
         # initial error
         pred = self.predict(x)
-        self.errors.append(self._sum_squared_error(pred, y))
+        self.errors.append(self._mean_squared_error(pred, y))
         for i in range(n_iter):
             # compute gradient for W and b
+
+            # partial derivative of error with regard to W
             if self._is_univariate:
-                #grad_W = lr * ((pred - y) * x).mean()
-                grad_W = lr * (1 / len(y)) * sum((pred  - y) * x)
+                grad_W = lr * (1 / len(y)) * sum((pred - y) * x)
             else:
                 grad_W = lr * (1 / len(y)) * (np.dot(pred - y, x))
+            # partial derivative of error with regard to b
             grad_b = lr * (1 / len(y)) * sum(pred - y)
+
             # update parameters
             self.W = self.W - grad_W
             self.b = self.b - grad_b
 
             pred = self.predict(x)
-            self.errors.append(self._sum_squared_error(pred, y))
+            self.errors.append(self._mean_squared_error(pred, y))
 
     def _is_converged(self):
         pass
 
-    def _mean_sum_squared_error(self, pred, y):
+    @staticmethod
+    def _mean_squared_error(pred, y):
         """compute the error of predicted values"""
-        #np.mean((pred - y) ** 2)
         return (1 / len(y)) * sum((pred - y) ** 2)
 
     def predict(self, x):
